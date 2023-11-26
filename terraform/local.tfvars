fbf49f0ed04b0575d2a08b58166ff9767b70cbcf
Jacques Garre <jacques@pepite-webagency.com>
2023-11-26 10:48:59 +0100

    mysql container

---
+++ terraform/local.tfvars
@@ -0,0 +1,4 @@
+mysql_root_password     = "root_password"
+mysql_database          = "fit_tracker_api_db"
+mysql_user              = "user"
+mysql_password          = "password"
--- terraform/main.tf
+++ terraform/main.tf
@@ -9,16 +9,36 @@ terraform {
 
 provider "docker" {}
 
+# Nginx container
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
 }
-
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "fit-tracker-api-nginx"
+  name  = "fit-tracker-api-nginx-container"
   ports {
     internal = 80
     external = 8080
   }
+}
+
+# MySQL container
+resource "docker_image" "mysql" {
+  name         = "mysql:latest"
+  keep_locally = false
+}
+resource "docker_container" "mysql" {
+  name  = "fit-tracker-api-mysql-container"
+  image = docker_image.mysql.image_id
+  ports {
+    internal = 3306
+    external = 3306
+  }
+  env = [
+    "MYSQL_ROOT_PASSWORD=${var.mysql_root_password}",
+    "MYSQL_PASSWORD=${var.mysql_password}",
+    "MYSQL_USER=${var.mysql_user}",
+    "MYSQL_DATABASE=${var.mysql_database}"
+  ]
 }
