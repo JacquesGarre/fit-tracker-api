4afa2bea0c2f6d3d41bae9888d1e9e85e8108052
Jacques <jcqs.gr@gmail.com>
2023-12-10 13:15:20 +0100

    adding id to jwt token

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FitTrackerApi\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        // Get the user from the event
+        $user = $event->getUser();
+
+        // Add custom claims to the token
+        $payload = $event->getData();
+        $payload['id'] = $user->getId(); 
+
+        // Update the token data
+        $event->setData($payload);
+    }
+}
