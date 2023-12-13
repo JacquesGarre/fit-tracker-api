7bd4f58126c3fc3119f4e901aaf9dc78f0a89f62
Jacques <jcqs.gr@gmail.com>
2023-12-13 08:55:21 +0100

    delete if exist

---
+++ src/EventListener/RecordEventListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FitTrackerApi\EventListener;
+
+use FitTrackerApi\Entity\Record;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+class RecordEventListener
+{
+    public function prePersist(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+        if (!$entity instanceof Record) {
+            return;
+        }
+        $entityManager = $args->getObjectManager();
+
+        $existingRecord = $entityManager->getRepository(Record::class)->findOneBy([
+            'unit' => $entity->getUnit(),
+            'user' => $entity->getUser(),
+            'workoutExercise' => $entity->getWorkoutExercise(),
+            'setId' => $entity->getSetId(),
+        ]);
+
+        if ($existingRecord) {
+            $entityManager->remove($existingRecord);
+            $entityManager->flush();
+        }
+
+    }
+}
